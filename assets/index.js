1cd4f36cac9cd6961bcea9815b733384e35f5c07
Tapupfr <lunadiffusion95@gmail.com>
2025-03-21 16:28:41 +0000

    Create index.js

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Tap Up Game Loaded 🎮");
+  document.getElementById("root").innerHTML =
+    "<h2 style='color:white;text-align:center;'>Le jeu est prêt !</h2>";
+});
